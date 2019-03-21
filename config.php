b9d1e7e35a430069e2977bfe45c85db7e99fef47
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-21 17:17:49 +0800

    优化插件内的快递单模板数据

--- config.php
+++ config.php
@@ -53,8 +53,29 @@ return array(
     'print_support' => true,
 	'print_model'	=> 2,			/* 模式编辑器 */
 	'print_bg'		=> 'images/dly_ems.jpg',			/* 打印单背景 */
-	'config_lable'	=> 't_shop_name,shop_name,236,32,182,161,b_shop_name||,||t_shop_tel,shop_tel,127,21,295,135,b_shop_tel||,||t_shop_address,shop_address,296,68,124,190,b_shop_address||,||t_pigeon,pigeon,21,21,192,278,b_pigeon||,||t_customer_name,customer_name,107,23,494,136,b_customer_name||,||t_customer_tel,customer_tel,155,21,639,124,b_customer_tel||,||t_customer_mobel,customer_mobel,159,21,639,147,b_customer_mobel||,||t_customer_post,customer_post,88,21,680,258,b_customer_post||,||t_year,year,37,21,534,379,b_year||,||t_months,months,29,21,592,379,b_months||,||t_day,day,27,21,642,380,b_day||,||t_order_best_time,order_best_time,104,39,688,359,b_order_best_time||,||t_order_postscript,order_postscript,305,34,485,402,b_order_postscript||,||t_customer_address,customer_address,289,48,503,190,b_customer_address||,||',			/* 打印快递单标签位置信息 */
-		
+//	'config_lable'	=> 't_shop_name,shop_name,236,32,182,161,b_shop_name||,||t_shop_tel,shop_tel,127,21,295,135,b_shop_tel||,||t_shop_address,shop_address,296,68,124,190,b_shop_address||,||t_pigeon,pigeon,21,21,192,278,b_pigeon||,||t_customer_name,customer_name,107,23,494,136,b_customer_name||,||t_customer_tel,customer_tel,155,21,639,124,b_customer_tel||,||t_customer_mobel,customer_mobel,159,21,639,147,b_customer_mobel||,||t_customer_post,customer_post,88,21,680,258,b_customer_post||,||t_year,year,37,21,534,379,b_year||,||t_months,months,29,21,592,379,b_months||,||t_day,day,27,21,642,380,b_day||,||t_order_best_time,order_best_time,104,39,688,359,b_order_best_time||,||t_order_postscript,order_postscript,305,34,485,402,b_order_postscript||,||t_customer_address,customer_address,289,48,503,190,b_customer_address||,||',			/* 打印快递单标签位置信息 */
+
+    /**
+     * 打印快递单标签位置信息
+     * 使用 ||,|| 合并连接
+     */
+    'config_lable'  => array(
+        't_shop_name,shop_name,236,32,182,161,b_shop_name',
+        't_shop_tel,shop_tel,127,21,295,135,b_shop_tel',
+        't_shop_address,shop_address,296,68,124,190,b_shop_address',
+        't_pigeon,pigeon,21,21,192,278,b_pigeon',
+        't_customer_name,customer_name,107,23,494,136,b_customer_name',
+        't_customer_tel,customer_tel,155,21,639,124,b_customer_tel',
+        't_customer_mobel,customer_mobel,159,21,639,147,b_customer_mobel',
+        't_customer_post,customer_post,88,21,680,258,b_customer_post',
+        't_year,year,37,21,534,379,b_year',
+        't_months,months,29,21,592,379,b_months',
+        't_day,day,27,21,642,380,b_day',
+        't_order_best_time,order_best_time,104,39,688,359,b_order_best_time',
+        't_order_postscript,order_postscript,305,34,485,402,b_order_postscript',
+        't_customer_address,customer_address,289,48,503,190,b_customer_address',
+    ),
+
 	'forms' => array(
 			array('name' => 'fee_compute_mode', 'type' => 'radiobox', 'value' => 'by_weight'), /* 费用计算方式  */
 			array('name' => 'item_fee',     'value' => 20),
@@ -87,6 +108,7 @@ return array(
             'shop_district'         => __('网店-区/县', 'ship_ems'),
             'shop_tel'              => __('网店-联系电话', 'ship_ems'),
             'shop_address'          => __('网店-地址', 'ship_ems'),
+
             'customer_country'      => __('收件人-国家', 'ship_ems'),
             'customer_province'     => __('收件人-省份', 'ship_ems'),
             'customer_city'         => __('收件人-城市', 'ship_ems'),
@@ -96,6 +118,7 @@ return array(
             'customer_post'         => __('收件人-邮编', 'ship_ems'),
             'customer_address'      => __('收件人-详细地址', 'ship_ems'),
             'customer_name'         => __('收件人-姓名', 'ship_ems'),
+
             'year'                  => __('年-当日日期', 'ship_ems'),
             'months'                => __('月-当日日期', 'ship_ems'),
             'day'                   => __('日-当日日期', 'ship_ems'),
@@ -103,7 +126,6 @@ return array(
             'order_postscript'      => __('备注-订单', 'ship_ems'),
             'order_best_time'       => __('送货时间-订单', 'ship_ems'),
             'pigeon'                => __('√-对号', 'ship_ems'),
-            //'custom_content' => '自定义内容',
         ),
 
         //模板文件
